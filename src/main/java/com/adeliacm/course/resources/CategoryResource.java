96d98edf432e950a31adca11db0e3e596acd948e
Adelia Camargo <adelia.camargo@br.ey.com>
2025-09-08 14:42:12 -0300

    Category

---
+++ src/main/java/com/adeliacm/course/resources/CategoryResource.java
@@ -0,0 +1,32 @@
+package com.adeliacm.course.resources;
+
+import com.adeliacm.course.entities.Category;
+import com.adeliacm.course.services.CategoryServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryResource {
+
+    @Autowired
+    private CategoryServices service;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll(){
+        List<Category> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findByID(@PathVariable Long id){
+        Category obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/adeliacm/course/services/CategoryServices.java
@@ -0,0 +1,25 @@
+package com.adeliacm.course.services;
+
+import com.adeliacm.course.entities.Category;
+import com.adeliacm.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class CategoryServices {
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+    public Category findById(Long id){
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
